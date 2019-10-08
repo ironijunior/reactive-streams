048985e24ac216c854cd46f2859e948c7671e038
ironijunior <ironimedina@gmail.com>
2019-10-08 19:14:36 -0300

    create flux and emitter

--- reactive-server/src/main/java/com/example/demo/DemoApplication.java
+++ reactive-server/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+//        SpringApplication.run(DemoApplication.class, args);
+
+        Flux.<Integer>create(DemoApplication::emitNumber, FluxSink.OverflowStrategy.BUFFER);
+
+    }
+
+    private static void emitNumber(FluxSink<Integer> integerFluxSink) {
     }
 }
